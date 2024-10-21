723517ce506eea51a675da929057b5c2c6ccb678
Laczovics Istvan <istvanl@topdesk.com>
2024-10-21 15:41:13 +0200

    Vuetify added

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <v-btn>
+     Button
+    </v-btn>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
